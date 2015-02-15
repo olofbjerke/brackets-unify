cdd2e19d9025113ec77661445454bfa1c85f4804
Olof Bjerke <olofbjerke@gmail.com>
2015-02-15 20:14:36 +0100

    Added possibility to sort selectors by specificity

--- main.js
+++ main.js
@@ -1,52 +1,17 @@
 /*jslint vars: true, plusplus: false, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
-/*global define, $, brackets, window */
+/*global define, $, brackets, window, SPECIFICITY */
 
 define(function (require, exports, module) {
     "use strict";
 
+    require("thirdparty/specificity/specificity");
     var CommandManager = brackets.getModule("command/CommandManager"),
         EditorManager  = brackets.getModule("editor/EditorManager"),
         KeyBindingManager = brackets.getModule("command/KeyBindingManager"),
         Menus          = brackets.getModule("command/Menus"),
         DocumentManager = brackets.getModule("document/DocumentManager"),
         CSSUtils       = brackets.getModule("language/CSSUtils");
-    
-   /*
-    FIRST .test SELECTOR
-    declListEndChar: 0
-    declListEndLine: 12
-    declListStartChar: 6
-    declListStartLine: 8
-    level: 0
-    parentSelectors: ""
-    ruleStartChar: 1
-    ruleStartLine: 8
-    selector: ".test"
-    selectorEndChar: 5
-    selectorEndLine: 8
-    selectorGroupStartChar: 0
-    selectorGroupStartLine: -1
-    selectorStartChar: 0
-    selectorStartLine: 8
 
-    BODY INSIDE MEDIA Query
-    declListEndChar: 4
-    declListEndLine: 4
-    1declListStartChar: 9
-    declListStartLine: 39
-    level: 1
-    parentSelectors: ""
-    ruleStartChar: 5
-    ruleStartLine: 39
-    selector: "body"
-    selectorEndChar: 8
-    selectorEndLine: 39
-    selectorGroupStartChar: 4
-    selectorGroupStartLine: -1
-    selectorStartChar: 4
-    selectorStartLine: 39
-    */
-    
     function filterSelectors(selectors, document) {
         var filteredSelectors = {},
             i;
@@ -99,17 +64,46 @@ define(function (require, exports, module) {
         return filteredSelectors;
     }
 
-    function sortSelectors(filteredSelectors) {
+    // selectors is a dictionary with unique selectors
+    function sortSelectorsByIndex(selectors) {
 
         var sortedSelectors = [],
             key;
 
-        for (key in filteredSelectors) {
-            if (filteredSelectors.hasOwnProperty(key)) {
-                var obj = filteredSelectors[key];
+        for (key in selectors) {
+            if (selectors.hasOwnProperty(key)) {
+                var obj = selectors[key];
                 sortedSelectors[obj.index] = obj;
             }
         }
+
+        return sortedSelectors;
+    }
+
+    // selectors is a dictionary with unique selectors
+    function sortSelectorsBySpecificity(selectors) {
+        var sortedSelectors = sortSelectorsByIndex(selectors);
+
+        function compareSpecificity(a, b) {
+            var i;
+            var s1 = SPECIFICITY.calculate(a.name);
+            var s2 = SPECIFICITY.calculate(b.name);
+
+            var a1 = s1[0].specificity.split(',');
+            var b1 = s2[0].specificity.split(',');
+
+            for (i = 0; i < a1.length; i = i + 1) {
+                if (a1[i] > b1[i]) {
+                    return 1;
+                } else if (a1[i] < b1[i]) {
+                    return -1;
+                }
+            }
+
+            return 0;
+        }
+
+        sortedSelectors.sort(compareSpecificity);
 
         return sortedSelectors;
     }
@@ -193,7 +187,7 @@ define(function (require, exports, module) {
     }
     
     // Function to run when the menu item is clicked
-    function unify() {
+    function unify(sortMethod) {
         var editor = EditorManager.getFocusedEditor();
         
         if (editor) {
@@ -202,19 +196,30 @@ define(function (require, exports, module) {
 
             var selectors = CSSUtils.extractAllSelectors(text);
             var filtered = filterSelectors(selectors, editor.document);
-            var sorted = sortSelectors(filtered);
+            var sorted = sortMethod(filtered);
             
             editor.document.setText(combineSelectors(sorted, editor.document));
         }
     }
 
+    function runIndex() {
+        unify(sortSelectorsByIndex);
+    }
+
+    function runSpecificity() {
+        unify(sortSelectorsBySpecificity);
+    }
+
     // First, register a command - a UI-less object associating an id to a handler
-    var MY_COMMAND_ID = "unifyCssSelectors.unify";   // package-style naming to avoid collisions
-    CommandManager.register("Unify all css selectors", MY_COMMAND_ID, unify);
-    KeyBindingManager.addBinding(MY_COMMAND_ID, "Shift-Ctrl-U");
+    var MY_COMMAND_ID1 = "olofbjerke.brackets-unify-css-selectors.index";
+    var MY_COMMAND_ID2 = "olofbjerke.brackets-unify-css-selectors.specificity";
+    CommandManager.register("Unify css selectors and sort by appearance", MY_COMMAND_ID1, runIndex);
+    CommandManager.register("Unify css selectors and sort by specificity", MY_COMMAND_ID2, runSpecificity);
+    KeyBindingManager.addBinding(MY_COMMAND_ID1, "Shift-Ctrl-U");
     
     // Then create a menu item bound to the command
     // The label of the menu item is the name we gave the command (see above)
     var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
-    menu.addMenuItem(MY_COMMAND_ID);
+    menu.addMenuItem(MY_COMMAND_ID1);
+    menu.addMenuItem(MY_COMMAND_ID2);
 });
