eadab566f45900ef85c22a389532e22b6d294ca6
Olof Bjerke <olofbjerke@gmail.com>
2015-02-16 21:43:34 +0100

    Added possibility to store media queries in sass variables

--- main.js
+++ main.js
@@ -5,30 +5,62 @@ define(function (require, exports, module) {
     "use strict";
 
     require("thirdparty_modules/specificity/specificity");
-    var CommandManager = brackets.getModule("command/CommandManager"),
-        EditorManager  = brackets.getModule("editor/EditorManager"),
-        KeyBindingManager = brackets.getModule("command/KeyBindingManager"),
-        Menus          = brackets.getModule("command/Menus"),
-        DocumentManager = brackets.getModule("document/DocumentManager"),
-        CSSUtils       = brackets.getModule("language/CSSUtils");
+    var CommandManager      = brackets.getModule("command/CommandManager"),
+        EditorManager       = brackets.getModule("editor/EditorManager"),
+        KeyBindingManager   = brackets.getModule("command/KeyBindingManager"),
+        Menus               = brackets.getModule("command/Menus"),
+        DocumentManager     = brackets.getModule("document/DocumentManager"),
+        CSSUtils            = brackets.getModule("language/CSSUtils");
+
+    var mediaQueries = [];
+    var convertToSass = false;
 
     function filterSelectors(selectors, document) {
         var filteredSelectors = {},
             i;
    
+        function mediaQuery(string) {
+            var key;
+
+            // Cleaning
+            string = string.toLowerCase();
+            string = string.replace('{', '');
+            string = string.replace('@media', '');
+            string = string.trim();
+
+            // Add to dictionary
+            if (mediaQueries[string]) {
+                key = mediaQueries[string].id;
+                mediaQueries[string].count = mediaQueries[string].count + 1;
+            } else {
+                mediaQueries[string] = {
+                    id: "$mediaQuery" + (Object.keys(mediaQueries).length + 1),
+                    query: string,
+                    count: 1
+                };
+
+                key = mediaQueries[string].id;
+            }
+
+            if (!convertToSass) {
+                key = string;
+            }
+
+            return key;
+        }
+
         function findMediaQuery(selector) {
             var line,
                 i;
             
             for (i = selector.selectorStartLine; i > 0; i = i - 1) {
                 line = document.getLine(i);
-                if (line.charAt(0) === '@') {
-                    return line;
+                if (line.charAt(0) === '@' && line.charAt(1).toLowerCase() === 'm') {
+                    return mediaQuery(line);
                 }
             }
             
             return null;
-            
         }
         
         function filterSelector(index, selector) {
@@ -86,16 +118,16 @@ define(function (require, exports, module) {
 
         function compareSpecificity(a, b) {
             var i;
-            var s1 = SPECIFICITY.calculate(a.name);
-            var s2 = SPECIFICITY.calculate(b.name);
+            a = SPECIFICITY.calculate(a.name);
+            b = SPECIFICITY.calculate(b.name);
 
-            var a1 = s1[0].specificity.split(',');
-            var b1 = s2[0].specificity.split(',');
+            a = a[0].specificity.split(',');
+            b = b[0].specificity.split(',');
 
-            for (i = 0; i < a1.length; i = i + 1) {
-                if (a1[i] > b1[i]) {
+            for (i = 0; i < a.length; i = i + 1) {
+                if (a[i] > b[i]) {
                     return 1;
-                } else if (a1[i] < b1[i]) {
+                } else if (a[i] < b[i]) {
                     return -1;
                 }
             }
@@ -113,7 +145,7 @@ define(function (require, exports, module) {
             selectorName,
             i;
         
-        function combineLines(selector, ranges) {
+        function combineCssLines(selector, ranges) {
             var text = "",
                 j,
                 k,
@@ -154,7 +186,12 @@ define(function (require, exports, module) {
             for (j = 0; j < ranges.length; j = j + 1) {
                 if (ranges[j].mediaQuery) {
                     
-                    text = text + ranges[j].mediaQuery + '\n';
+                    if (convertToSass) {
+                        text = text + "@media #{" + ranges[j].mediaQuery + '} {\n';
+                    } else {
+                        text = text + "@media " + ranges[j].mediaQuery + ' {\n';
+                    }
+
                     text = text + '\t' + selector;
                     
                     start = ranges[j].lineStart;
@@ -175,11 +212,127 @@ define(function (require, exports, module) {
             return text;
         }
         
+        function combineSassLines(selector, ranges) {
+            var text = "",
+                j,
+                k,
+                line,
+                start,
+                end,
+                baseExists = false;
+
+            // Check if base selector exists
+            // Otherwise the base selector will be empty
+            for (j = 0; j < ranges.length; j = j + 1) {
+                if (!ranges[j].mediaQuery) {
+                    baseExists = true;
+                }
+            }
+
+            if (baseExists) {
+                // Start of selector
+                text = text + selector;
+
+                // Create the base first time
+                for (j = 0; j < ranges.length; j = j + 1) {
+                    if (!ranges[j].mediaQuery) {
+                        start = ranges[j].lineStart;
+                        end = ranges[j].lineEnd;
+                        for (k = start + 1; k <= end - 1; k = k + 1) {
+                            line = document.getLine(k);
+                            text = text + line + "\n";
+                        }
+                    }
+                }
+
+                // Create the Media queries second time
+                for (j = 0; j < ranges.length; j = j + 1) {
+                    if (ranges[j].mediaQuery) {
+
+                        if (convertToSass) {
+                            text = text + "\n\t@media #{" + ranges[j].mediaQuery + '} {\n';
+                        } else {
+                            text = text + "\t@media " + ranges[j].mediaQuery + ' {\n';
+                        }
+
+                        text = text + '\t\t& {\n';
+
+                        start = ranges[j].lineStart;
+                        end = ranges[j].lineEnd;
+                        for (k = start + 1; k <= end - 1; k = k + 1) {
+                            line = document.getLine(k).trim();
+                            text = text + '\t\t\t' + line + "\n";
+                        }
+
+                        // Close selector
+                        text = text + '\t\t}\n';
+                        // Close media query
+                        text = text + '\t}\n';
+                    }
+                }
+
+                // End Base
+                text = text + '}\n\n';
+            } else {
+                // Create the Media queries second time
+                for (j = 0; j < ranges.length; j = j + 1) {
+                    if (ranges[j].mediaQuery) {
+
+                        if (convertToSass) {
+                            text = text + "@media #{" + ranges[j].mediaQuery + '} {\n';
+                        } else {
+                            text = text + "@media " + ranges[j].mediaQuery + ' {\n';
+                        }
+
+                        text = text + '\t' + selector;
+
+                        start = ranges[j].lineStart;
+                        end = ranges[j].lineEnd;
+                        for (k = start + 1; k <= end - 1; k = k + 1) {
+                            line = document.getLine(k).trim();
+                            text = text + '\t\t' + line + "\n";
+                        }
+
+                        // Close selector
+                        text = text + '\t}\n';
+
+                        // Close media query
+                        text = text + '}\n\n';
+                    }
+                }
+            }
+
+            return text;
+        }
+
+        function printMediaQueries() {
+            var key,
+                text = "";
+
+            for (key in mediaQueries) {
+                if (mediaQueries.hasOwnProperty(key)) {
+                    var obj = mediaQueries[key];
+                    text = text + obj.id + ": \"" + obj.query + "\"; \/\/ used " + obj.count + " times\n";
+                }
+            }
+
+            return text;
+        }
+
+        if (convertToSass) {
+            documentText = documentText + printMediaQueries() + "\n\n";
+        }
+
         for (i = 0; i < selectors.length; i = i + 1) {
             // Might have empty indexes
             if (selectors[i]) {
                 selectorName = selectors[i].name + " { \n";
-                documentText = documentText + combineLines(selectorName, selectors[i].ranges);
+
+                if (convertToSass) {
+                    documentText = documentText + combineSassLines(selectorName, selectors[i].ranges);
+                } else {
+                    documentText = documentText + combineCssLines(selectorName, selectors[i].ranges);
+                }
             }
         }
 
@@ -191,6 +344,8 @@ define(function (require, exports, module) {
         var editor = EditorManager.getFocusedEditor();
         
         if (editor) {
+            // Reset list of media queries
+            mediaQueries = [];
             var insertionPos = editor.getCursorPos();
             var text = editor.document.getText();
 
@@ -202,24 +357,34 @@ define(function (require, exports, module) {
         }
     }
 
+    function runIndexAndConvertToSass() {
+        convertToSass = true;
+        unify(sortSelectorsByIndex);
+    }
+
     function runIndex() {
+        convertToSass = false;
         unify(sortSelectorsByIndex);
     }
 
     function runSpecificity() {
+        convertToSass = false;
         unify(sortSelectorsBySpecificity);
     }
 
     // First, register a command - a UI-less object associating an id to a handler
-    var MY_COMMAND_ID1 = "olofbjerke.brackets-unify-css-selectors.index";
-    var MY_COMMAND_ID2 = "olofbjerke.brackets-unify-css-selectors.specificity";
-    CommandManager.register("Unify css selectors and sort by appearance", MY_COMMAND_ID1, runIndex);
-    CommandManager.register("Unify css selectors and sort by specificity", MY_COMMAND_ID2, runSpecificity);
-    KeyBindingManager.addBinding(MY_COMMAND_ID1, "Shift-Ctrl-U");
+    var COMMAND_1 = "olofbjerke.brackets-unify-css-selectors.index";
+    var COMMAND_2 = "olofbjerke.brackets-unify-css-selectors.specificity";
+    var COMMAND_3 = "olofbjerke.brackets-unify-css-selectors.sass";
+    CommandManager.register("Unify css selectors and sort by appearance", COMMAND_1, runIndex);
+    CommandManager.register("Unify css selectors and sort by specificity", COMMAND_2, runSpecificity);
+    CommandManager.register("Unify css selectors and convert to Sass", COMMAND_3, runIndexAndConvertToSass);
+    KeyBindingManager.addBinding(COMMAND_1, "Shift-Ctrl-U");
     
     // Then create a menu item bound to the command
     // The label of the menu item is the name we gave the command (see above)
     var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
-    menu.addMenuItem(MY_COMMAND_ID1);
-    menu.addMenuItem(MY_COMMAND_ID2);
+    menu.addMenuItem(COMMAND_1);
+    menu.addMenuItem(COMMAND_2);
+    menu.addMenuItem(COMMAND_3);
 });
